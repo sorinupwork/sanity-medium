d13229b486c86aa098860a86cbf6121d50b693df
sorinupwork <sorin.upwork@gmail.com>
2023-06-20 18:13:09 +0300

    change to ts

--- sanity-medium-app/schemas/comment.ts
+++ sanity-medium-app/schemas/comment.ts
@@ -1,30 +1,36 @@
-export default {
+import {defineField, defineType} from 'sanity'
+
+export default defineType({
   name: 'comment',
-  type: 'document',
   title: 'Comment',
+  type: 'document',
   fields: [
-    {
+    defineField({
       name: 'name',
+      title: 'Name',
       type: 'string',
-    },
-    {
-      title: 'Approved',
+    }),
+    defineField({
       name: 'approved',
+      title: 'Approved',
       type: 'boolean',
       description: "Comments won't show on the site without approval",
-    },
-    {
+    }),
+    defineField({
       name: 'email',
+      title: 'Email',
       type: 'string',
-    },
-    {
+    }),
+    defineField({
       name: 'comment',
+      title: 'Comment',
       type: 'text',
-    },
-    {
+    }),
+    defineField({
       name: 'post',
+      title: 'Post',
       type: 'reference',
       to: [{type: 'post'}],
-    },
+    }),
   ],
-}
+})
